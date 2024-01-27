df07c8281c31bfed6d43129ef28294c1c69e5ddd
Dasha Zhebko <daryna.zhebko@gmail.com>
2024-01-27 18:29:33 +0200

    Initial commit

---
+++ src/main/java/org/example/homework21apiglovodb/HomeWork21ApiGlovoDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.homework21apiglovodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork21ApiGlovoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork21ApiGlovoDbApplication.class, args);
+	}
+
+}
